3e9cb749a385a536ecf5fe60d2dffe716220e5fc
ravipikachupica1 <38157930+ravipikachupica1@users.noreply.github.com>
2019-12-06 18:17:11 +0530

    Update index.php

--- main/index.php
+++ main/index.php
@@ -2,7 +2,7 @@
 <html lang="en-US">
 
 
-<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<meta http-equiv="content-type" cdhxdhdxontent="text/html;charset=utf-8" />
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
